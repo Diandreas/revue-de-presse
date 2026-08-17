d2df72f41c0f30cf5265cfcebf3a982446154d7b
David Andreas MIR <127276266+Diandreas@users.noreply.github.com>
2026-08-17 11:16:57 +0100

    Create apps.py

---
+++ backend/apps/ai_pipeline/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class AiPipelineConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.ai_pipeline"
+    label = "ai_pipeline"
+    verbose_name = "Pipeline IA"
